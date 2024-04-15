7d870601c08c888fead9e2d15be6039bc585696d
Jessie Shi <jessie.shi@me.com>
2024-04-15 16:32:50 +0100

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-       Before the world was opened, the sky and the earth were a chaotic mass, like an egg, and Pangu was born among them.
+        Before the world was opened, the sky and the earth were a chaotic mass,
+        like an egg, and Pangu was born among them.
       </div>
 
       <div className={styles.center}>
-        <h1>Hello Pangu</h1>
+        <h1>Hello Pangu 2.0</h1>
+        <P>relive a new life in lengendary era.</P>
         <Image
           src="/amplify.svg"
           alt="Amplify Logo"
@@ -26,9 +28,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2>
-            Open Sky
-          </h2>
+          <h2>Open Sky</h2>
           <p>The light and clear Yang Qi ascended to become the sky</p>
         </a>
 
@@ -54,10 +54,15 @@ export default function Home() {
             The world is born <span>-&gt;</span>
           </h2>
           <p>
-            Pangu was between heaven and earth. He changed many times a day and was more sacred than heaven and earth. Every day the sky rose ten feet higher, the earth became ten feet thicker every day, and Pangu grew ten feet taller every day. This continued for another eighteen thousand years. The sky rose very high, the earth sank very deep, and Pangu grew very tall. 
+            Pangu was between heaven and earth. He changed many times a day and
+            was more sacred than heaven and earth. Every day the sky rose ten
+            feet higher, the earth became ten feet thicker every day, and Pangu
+            grew ten feet taller every day. This continued for another eighteen
+            thousand years. The sky rose very high, the earth sank very deep,
+            and Pangu grew very tall.
           </p>
         </a>
       </div>
     </main>
-  )
+  );
 }
